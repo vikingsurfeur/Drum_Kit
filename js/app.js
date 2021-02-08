fc7f294dbea50949b10cae281589d71d3314a328
David Bouscarle <d.bouscarle@gmail.com>
2021-02-08 11:30:41 +0100

    update before Lunch 08/02/2021

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
+const drums = new Howl({
+    src: ["./audio/drums.webm", "./audio/drums.mp3"],
+    sprite: {
+        clap: [0, 1365.578231292517],
+        closed_hi_hat: [3000, 961.6326530612245],
+        crash: [5000, 7569.750566893424],
+        kick: [14000, 1375.9863945578238],
+        open_hi_hat: [17000, 1061.9501133786855],
+        snare: [20000, 1260.2947845804983],
+    },
+});
 
-
+drums.play('kick');
